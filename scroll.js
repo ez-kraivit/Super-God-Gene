c3919f486efc74c713751ec039eed8532be2d935
ez-kraivit <kraivit.m@ku.th>
2020-04-01 20:14:06 +0700

    Super God Gene all

---
+++ scroll.js
@@ -0,0 +1,51 @@
+function topFunction() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div0").offset().top
+        }, 200)
+    });
+
+}
+function bomFunction() {
+    $('div.div0').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div2").offset().top
+        }, 200)
+    });
+
+}
+function bomFunction2() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div2").offset().top
+        }, 200)
+    });
+}
+function bomFunction3() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div4").offset().top
+        }, 200)
+    });
+}
+function bomFunction4() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div5").offset().top
+        }, 200)
+    });
+}
+function bomFunction5() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div6").offset().top
+        }, 200)
+    });
+}
+function bomFunction6() {
+    $('div.div2').click(function () {
+        $('html, body').animate({
+            scrollTop: $("div.div7").offset().top
+        }, 200)
+    });
+}
---
+++ switch_on.js
@@ -0,0 +1,61 @@
+jQuery(function () {
+    var c1 = 0;
+    jQuery("#d1").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s1").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s1").style.display = '';
+        }
+    });
+    jQuery("#d2").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s2").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s2").style.display = '';
+        }
+    });
+    jQuery("#d3").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s3").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s3").style.display = '';
+        }
+    });
+    jQuery("#d4").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s4").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s4").style.display = '';
+        }
+    });
+    jQuery("#d5").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s5").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s5").style.display = '';
+        }
+    });
+    jQuery("#d6").click(function () {
+        if (c1 == 1) {
+            c1 = 0;
+            document.getElementById("s6").style.display = 'none';
+        } else {
+            c1 = 1;
+            document.getElementById("s6").style.display = '';
+        }
+    });
+
+
+
+});
+// ทั้งหมด
